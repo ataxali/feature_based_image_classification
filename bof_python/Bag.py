ce5d8999d156c4e315cb6ba67405176d423a0be9
Aman Taxali <ataxali@umich.edu>
2018-04-15 00:09:42 -0400

    adding hi res images

--- bof_python/Bag.py
+++ bof_python/Bag.py
@@ -1,9 +1,19 @@
 import cv2
 import numpy as np
 from glob import glob
+import mnist
 import argparse
 from helpers import *
 from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+
+
+class_hog_vectors = dict()
+class_hog_means = dict()
+class_hog_vars = dict()
+HOG_PCA_COMPONENTS = 100
+class_hog_eig_vectors = dict()
 
 
 class BOV:
@@ -31,31 +41,52 @@ class BOV:
 
         # read file. prepare file lists.
         self.images, self.trainImageCount = self.file_helper.getFiles(
-            self.train_path)
+            self.train_path, limit=50)
+        #self.images, self.trainImageCount = mnist.get_data("training", 1000)
         # extract SIFT Features from each image
         label_count = 0
         for word, imlist in self.images.items():
             self.name_dict[str(label_count)] = word
-            print("Computing Features for ", word)
+            print("Computing Features for", word)
             for im in imlist:
                 # cv2.imshow("im", im)
                 # cv2.waitKey()
                 self.train_labels = np.append(self.train_labels, label_count)
                 kp, des = self.im_helper.features(im)
                 self.descriptor_list.append(des)
+                if word in class_hog_vectors:
+                    class_hog_vectors[word].append(des.tolist())
+                else:
+                    class_hog_vectors[word] = [des.tolist()]
 
             label_count += 1
 
-        # perform clustering
+        ##################
+        # HOG-PCA Method
+        ##################
+        '''
+        for c in class_hog_vectors.keys():
+            pca = PCA(n_components=HOG_PCA_COMPONENTS, svd_solver='full')
+            c_vecs = class_hog_vectors[c]
+            c_X = np.array(c_vecs[0])
+            for remaining in c_vecs[1:]:
+                c_X = np.vstack((c_X, remaining))
+            class_hog_means[c] = np.mean(c_X, axis=0)
+            class_hog_vars[c] = np.var(c_X, axis=0)
+            c_X = preprocessing.scale(c_X, with_mean=True, with_std=True, axis=0)
+            pca.fit(c_X)
+            class_hog_eig_vectors[c] = pca
+        '''
+        #####################
+        # Bag of Features
+        #####################
+        # perform k-means clustering
         bov_descriptor_stack = self.bov_helper.formatND(self.descriptor_list)
         self.bov_helper.cluster()
         self.bov_helper.developVocabulary(n_images=self.trainImageCount,
                                           descriptor_list=self.descriptor_list)
-
         # show vocabulary trained
-        # self.bov_helper.plotHist()
-
-
+        self.bov_helper.plotHist()
         self.bov_helper.standardize()
         self.bov_helper.train(self.train_labels)
 
@@ -69,8 +100,48 @@ class BOV:
         """
 
         kp, des = self.im_helper.features(test_img)
+
+        if des is None: return ["Error"]
+        ###################
+        # hog-pca method
+        ###################
+        '''
+        distances = dict()
+        for c in class_hog_eig_vectors:
+            c_X = np.array(des[0]) - class_hog_means[c]
+            #with np.errstate(divide='ignore', invalid='ignore'):
+            #    std_devs = np.sqrt(class_hog_vars[c])
+            #    c_X = np.divide(c_X, std_devs, where=std_devs!=0)
+            #c_X = np.nan_to_num(c_X)
+            pca_proj = class_hog_eig_vectors[c].transform(np.resize(c_X, (1, len(c_X))))
+            #pca_mean_proj = class_hog_eig_vectors[c].transform(np.resize(class_hog_means[c],
+            #                                                             (1, len(class_hog_means[c]))))
+            pca_inv_proj = class_hog_eig_vectors[c].inverse_transform(pca_proj)
+            distances[c] = np.linalg.norm(des - (pca_inv_proj + class_hog_means[c]))
+
+        # print(distances)
+
+        min_dist = np.Inf
+        min_class = None
+        for c in distances:
+            if distances[c] < min_dist:
+                min_dist = distances[c]
+                min_class = c
+
+        for id, name in self.name_dict.items():
+            if name == min_class:
+                return id
+
+        if min_class is None:
+            return ['Error']
+        '''
+
+
+        ###########################
+        # Bag of features method
+        ###########################
         # print kp
-        print(des.shape)
+        #print(des.shape)
 
         # generate vocab for test image
         vocab = np.array([[0 for i in range(self.no_clusters)]])
@@ -85,7 +156,7 @@ class BOV:
         for each in test_ret:
             vocab[0][each] += 1
 
-        print(vocab)
+        #print(vocab)
         # Scale the features
         vocab = self.bov_helper.scale.transform(vocab)
 
@@ -104,32 +175,49 @@ class BOV:
         """
 
         self.testImages, self.testImageCount = self.file_helper.getFiles(
-            self.test_path)
+            self.test_path, limit=50)
+        #self.testImages, self.testImageCount = mnist.get_data("testing", 100)
 
         predictions = []
+        true_values = []
 
         for word, imlist in self.testImages.items():
             print("processing ", word)
             for im in imlist:
                 # print imlist[0].shape, imlist[1].shape
-                print(im.shape)
+                #print(im.shape)
                 cl = self.recognize(im)
-                print(cl)
+                if cl[0] == "Error":
+                    print("Could not classify ", word, im)
+                    continue
+                #print(cl)
                 predictions.append({
                     'image': im,
                     'class': cl,
                     'object_name': self.name_dict[str(int(cl[0]))]
                 })
+                true_values.append(word)
 
-        print(predictions)
-        for each in predictions:
+        # print(predictions)
+        incorrect_classifications = 0
+        correct_classifications = 0
+        for i, each in enumerate(predictions):
             # cv2.imshow(each['object_name'], each['image'])
             # cv2.waitKey()
             # cv2.destroyWindow(each['object_name'])
-            # 
-            plt.imshow(cv2.cvtColor(each['image'], cv2.COLOR_GRAY2RGB))
-            plt.title(each['object_name'])
-            plt.show()
+            #
+            if each['object_name'] != true_values[i]:
+                #plt.imshow(cv2.cvtColor(each['image'], cv2.COLOR_GRAY2RGB))
+                #plt.title(each['object_name'])
+                #plt.show()
+                incorrect_classifications += 1
+            else:
+                correct_classifications += 1
+
+        print("Correct classifications", correct_classifications)
+        print("Incorrect classifications", incorrect_classifications)
+        print("Err pct", incorrect_classifications/(correct_classifications + incorrect_classifications))
+
 
     def print_vars(self):
         pass
@@ -148,7 +236,7 @@ if __name__ == '__main__':
     args = vars(parser.parse_args())
     print(args)
 
-    bov = BOV(no_clusters=100)
+    bov = BOV(no_clusters=200)
 
     # set training paths
     bov.train_path = args['train_path']
--- bof_python/helpers.py
+++ bof_python/helpers.py
@@ -5,25 +5,85 @@ from sklearn.cluster import KMeans
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot as plt
+from skimage.feature import hog
+from skimage import data, exposure
+import time
+
+
+
 
 
 class ImageHelpers:
     def __init__(self):
-        self.sift_object = cv2.xfeatures2d.SIFT_create()
+        self.sift_object = cv2.xfeatures2d.SIFT_create(nfeatures=100)
+        #self.sift_object = cv2.xfeatures2d.SIFT_create(nfeatures=30)
+        #self.sift_object = cv2.xfeatures2d.SIFT_create(nfeatures=10, sigma=1, edgeThreshold=100)
 
     def gray(self, image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         return gray
 
     def features(self, image):
+        ###################
+        # SIFT
+        ###################
         keypoints, descriptors = self.sift_object.detectAndCompute(image, None)
+        #new_img = cv2.drawKeypoints(image, keypoints, None,
+        #                        flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        # cv2.imwrite('sift_keypoints.jpg', new_img)
+
+        key_points = np.zeros((image.shape[0], image.shape[1]))
+        for keypoint in keypoints:
+            kp_center = (int(round(keypoint.pt[0])), int(round(keypoint.pt[1])))
+            kp_radius = int(round(keypoint.size))
+            for i in range(kp_center[1]-kp_radius, kp_center[1]+kp_radius):
+                y_cord = np.minimum(i, image.shape[1]-1)
+                x_lo_cord = np.maximum(kp_center[0] - kp_radius, 0)
+                x_hi_cord = np.minimum(kp_center[0] + kp_radius, image.shape[0]-1)
+                key_points[y_cord, x_lo_cord:x_hi_cord] = \
+                    image[y_cord, x_lo_cord:x_hi_cord]
+        # cv2.imwrite("keypoints_only.jpg", key_points)
+
+        ####################
+        # HOG
+        ####################
+        descriptors, hog_img = hog(key_points, orientations=8, pixels_per_cell=(12, 12),
+                                   cells_per_block=(2, 2),
+                                   visualise=True, block_norm='L2')
+
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 4),
+                                       sharex=True,
+                                       sharey=True)
+        ax1.axis('off')
+        ax1.imshow(image, cmap=plt.cm.gray)
+        ax1.set_title('Input image')
+
+        ax2.axis('off')
+        ax2.imshow(key_points, cmap=plt.cm.gray)
+        ax2.set_title('SIFT Features')
+
+        hog_image_rescaled = exposure.rescale_intensity(hog_img,
+                                                       in_range=(0, 10))
+        ax3.axis('off')
+        ax3.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+        ax3.set_title('Histogram of Oriented Gradients')
+        plt.show()
+
+        descriptors = np.array(descriptors)
+        non_zero_descriptors = np.nonzero(descriptors)
+        non_zero_descriptors = np.dstack((non_zero_descriptors, descriptors[non_zero_descriptors]))
+        non_zero_descriptors = np.squeeze(non_zero_descriptors, axis=0)
+
+        descriptors = non_zero_descriptors
+        keypoints = non_zero_descriptors.shape[0]
+
         return [keypoints, descriptors]
 
 
 class BOVHelpers:
     def __init__(self, n_clusters=20):
         self.n_clusters = n_clusters
-        self.kmeans_obj = KMeans(n_clusters=n_clusters)
+        self.kmeans_obj = KMeans(n_clusters=n_clusters, verbose=False, n_jobs=1)
         self.kmeans_ret = None
         self.descriptor_vstack = None
         self.mega_histogram = None
@@ -34,7 +94,10 @@ class BOVHelpers:
         cluster using KMeans algorithm,
 
         """
+        start = time.time()
+        print("Clustering...")
         self.kmeans_ret = self.kmeans_obj.fit_predict(self.descriptor_vstack)
+        print("Done clustering...", time.time()-start, "seconds")
 
     def developVocabulary(self, n_images, descriptor_list, kmeans_ret=None):
 
@@ -48,14 +111,20 @@ class BOVHelpers:
         all descriptions for all images
 
         """
-
+        print("Creating vocabulary histogram...")
         self.mega_histogram = np.array(
             [np.zeros(self.n_clusters) for i in range(n_images)])
         old_count = 0
         for i in range(n_images):
-            l = len(descriptor_list[i])
+            if descriptor_list[i] is None:
+                l = 0
+            else:
+                l = len(descriptor_list[i])
             for j in range(l):
                 if kmeans_ret is None:
+                    if old_count + j == len(self.kmeans_ret):
+                        print(j, l)
+                        old_count -= 1
                     idx = self.kmeans_ret[old_count + j]
                 else:
                     idx = kmeans_ret[old_count + j]
@@ -85,9 +154,16 @@ class BOVHelpers:
 
         """
         vStack = np.array(l[0])
+        counter = 0
         for remaining in l[1:]:
+            counter += 1
+            if remaining is None:
+                continue
             vStack = np.vstack((vStack, remaining))
+            if not (counter % 1000):
+                print("Reshaping feature row", counter)
         self.descriptor_vstack = vStack.copy()
+        print("Stack formatting completed...")
         return vStack
 
     def train(self, train_labels):
@@ -130,7 +206,7 @@ class FileHelpers:
     def __init__(self):
         pass
 
-    def getFiles(self, path):
+    def getFiles(self, path, limit=None):
         """
         - returns  a dictionary of all files
         having key => value as  objectname => image path
@@ -142,12 +218,22 @@ class FileHelpers:
         count = 0
         for each in glob(path + "*"):
             word = each.split("\\")[-1]
-            print(" #### Reading image category ", word, " ##### ")
+            print("#### Reading image category", word, "#####")
             imlist[word] = []
             for imagefile in glob(path + word + "/*"):
-                print("Reading file ", imagefile)
+                #print("Reading file ", imagefile)
                 im = cv2.imread(imagefile, 0)
-                imlist[word].append(im)
+                height, width = im.shape
+                if height != 300 or width != 400:
+                    # print("Bad file", imagefile, "height:", height, "width:", width)
+                    im = cv2.resize(im, (300, 400))
+                if limit:
+                    if len(imlist[word]) >= limit:
+                        continue
+                    else:
+                        imlist[word].append(im)
+                else:
+                    imlist[word].append(im)
                 count += 1
 
         return [imlist, count]
